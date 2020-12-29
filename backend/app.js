e911f6649e8c6c9e4093087e7ddcd46bfd34fe33
Juan Carlos Choque Quispe <juan.c.c.q@gmail.com>
2020-12-29 08:18:05 -0400

    added some changes on envfile

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const { Sequelize } = require("sequelize");
+
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
 	res.send("Project started");
 });
+
+console.log("ip: " + process.env.DATABASE_HOST);
+
+const sequelize = new Sequelize(
+	process.env.DATABASE_NAME,
+	process.env.DATABASE_USER,
+	process.env.DATABASE_PASS,
+	{
+		host: process.env.DATABASE_HOST,
+		dialect: "mysql",
+	}
+);
+
+const connectDb = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log("Connection successfully established");
+	} catch (err) {
+		console.log("Unable to connect to database: ", err);
+	}
+};
+
+connectDb();
 
 app.listen(port, () => {
 	console.log(`App running on ${port}`);
